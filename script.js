d3d286c98c27541f7932d38cfa0a7980d6b0f0b9
gustavo-me <g.bianchinimoraes@gmail.com>
2023-10-20 10:37:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+
+   //if(html.classList.contains('light')) {
+   //  html.classList.remove('light')
+   //} else {
+   //  html.classList.add('light')
+   //}
+
+   /* código mais simplificado */
+   html.classList.toggle('light')
+
+}
